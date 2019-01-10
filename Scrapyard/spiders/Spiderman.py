a2b90017aed80f600b1ce14d3ba38e7f28020975
Pegasus <pegasus.xiong@uhubor.com>
2019-01-10 14:42:22 -0800

    Crawl prof names of Stanford

--- Scrapyard/spiders/Spiderman.py
+++ Scrapyard/spiders/Spiderman.py
@@ -1,23 +1,25 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class ProfSpider(scrapy.Spider):
+    name = "prof"
 
     def start_requests(self):
-        url = 'http://quotes.toscrape.com/'
-        tag = getattr(self, 'tag', None)
-        if tag is not None:
-            url = url + 'tag/' + tag
+        url = 'https://engineering.stanford.edu/people/faculty/grid'
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-            }
+        """Parse index page."""
+        prof_urls = response.xpath('//*[contains(@class, "field-content")]//a/@href').extract()
+        for prof_url in prof_urls:
+            yield response.follow(prof_url, self.parse_prof)
 
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        # yield response.follow(prof_urls[0], self.parse_prof)
+
+        # with open('.\\data\\data.jl', 'w') as f:
+        #     f.write(str(prof_urls))
+
+    def parse_prof(self, response):
+        """Parse detail page."""
+        name = response.xpath('//h1/text()').extract()
+        yield {'name': name[0].strip()}
